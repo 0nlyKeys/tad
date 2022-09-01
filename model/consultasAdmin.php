740de6f66cc0dd311e2c080bc638649c961454f4
0nlyKeys <danielantonior1198@gmail.com>
2022-09-01 16:14:25 -0500

    Actualización rol admin registrar y editar

--- model/consultasAdmin.php
+++ model/consultasAdmin.php
@@ -2,7 +2,7 @@
 
     class ConsultasAdmin{
 
-        public function registrarUserE($identificacion, $tipoDoc, $nombres, $apellidos, $email, $telefono, $claveMd, $rol, $estado){
+        public function registrarUserE($identificacion, $tipoDoc, $nombres, $apellidos,$fechaNac, $email, $telefono, $ciudad, $localidad, $direccion, $postal, $claveMd, $rol, $estado){
 
             // Conectamos con la clase Conexion
             $objetoConexion = new Conexion();
@@ -28,8 +28,8 @@
                 $objetoConexion = new Conexion();
                 $conexion = $objetoConexion->get_conexion();
 
-                $sql = "INSERT INTO users (identificacion, tipodoc, nombres, apellidos, email, telefono, clave, rol, estado) 
-                VALUES(:identificacion, :tipoDoc,:nombres, :apellidos, :email, :telefono, :claveMd, :rol, :estado)";
+                $sql = "INSERT INTO users (identificacion, tipodoc, nombres, apellidos, fecha_nacimiento, email, telefono, ciudad, localidad, direccion, codigo_postal, clave, rol, estado) 
+                VALUES(:identificacion, :tipoDoc,:nombres, :apellidos, :fechaNac, :email, :telefono, :ciudad, :localidad, :direccion, :code_postal, :claveMd, :rol, :estado)";
 
                 $result = $conexion->prepare($sql);
 
@@ -37,8 +37,13 @@
                 $result->bindParam(':tipoDoc', $tipoDoc);
                 $result->bindParam(':nombres', $nombres);
                 $result->bindParam(':apellidos', $apellidos);
+                $result->bindParam(':fechaNac', $fechaNac);
                 $result->bindParam(':email', $email);
                 $result->bindParam(':telefono', $telefono);
+                $result->bindParam(':ciudad', $ciudad);
+                $result->bindParam(':localidad', $localidad);
+                $result->bindParam(':direccion', $direccion);
+                $result->bindParam(':code_postal', $postal);
                 $result->bindParam(':claveMd', $claveMd);
                 $result->bindParam(':rol', $rol);
                 $result->bindParam(':estado', $estado);
