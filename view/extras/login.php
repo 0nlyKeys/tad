1ab53137c6e4a0946890ca23ae777f80112cef00
0nlyKeys <danielantonior1198@gmail.com>
2022-10-20 22:58:42 -0500

    Modal del login

--- view/extras/login.php
+++ view/extras/login.php
@@ -63,17 +63,20 @@
         <!-- Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">¿Como desea registrarse?</h5>
-                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+            <div class="modal-content modal-login">
+              <div class="modal-header" style="border:none;">
+                <h5 class="modal-title" id="exampleModalLabel">¿Como quiere registrarse?</h5>
+                <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close" style="border: none;"><i class="bi bi-x-lg"></i></button>
               </div>
               <div class="modal-body">
-                <div class="row">
-                  <div class="user_modal_option col-md-6">
+                <div class="row options-modal-form">
+                  <div class="col-md-5 options-modal-btn">
                     <a href="./register-user.php">Usuario</a>
                   </div>
-                  <div class="user_modal_option col-md-6">
+                  <div class="col-md-2 options-modal-btn">
+                    o
+                  </div>
+                  <div class="col-md-5 options-modal-btn">
                     <a href="./register-tecnico.php">Técnico</a>
                   </div>
                 </div>
